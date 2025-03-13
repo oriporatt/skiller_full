1898bd0eae9df8922437909ed17320629c8b1a5e
oriporatt <oriporatt@gmail.com>
2025-03-13 11:40:06 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser'
 import { authRoutes } from './api/auth/auth.routes.js'
 import { userRoutes } from './api/user/user.routes.js'
 import { reviewRoutes } from './api/review/review.routes.js'
-import { carRoutes } from './api/car/car.routes.js'
+import { carRoutes } from './api/gig/gig.routes.js'
 import { setupSocketAPI } from './services/socket.service.js'
 
 import { setupAsyncLocalStorage } from './middlewares/setupAls.middleware.js'
@@ -36,10 +36,10 @@ if (process.env.NODE_ENV === 'production') {
 
 app.all('*', setupAsyncLocalStorage)
 
-app.use('/api/auth', authRoutes)
-app.use('/api/user', userRoutes)
-app.use('/api/review', reviewRoutes)
-app.use('/api/car', carRoutes)
+// app.use('/api/auth', authRoutes)
+// app.use('/api/user', userRoutes)
+// app.use('/api/review', reviewRoutes)
+// app.use('/api/car', carRoutes)
 
 setupSocketAPI(server)
 
