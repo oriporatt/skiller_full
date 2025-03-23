f19e4510e9b5fbe8d396d45d26d484a9f3f0f219
oriporatt <oriporatt@gmail.com>
2025-03-23 15:53:53 +0200

    updated bug

--- frontend/src/serviceWorkerRegistration.js
+++ frontend/src/serviceWorkerRegistration.js
@@ -30,8 +30,9 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}./service-worker.js`;
-  
+        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+        console.log(swUrl)
+
         if (isLocalhost) {
           // This is running on localhost. Let's check if a service worker still exists or not.
           checkValidServiceWorker(swUrl, config);
