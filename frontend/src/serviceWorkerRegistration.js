0bbd5eee0d5427ae168dc2c9fc07fe5a4e5d8a89
oriporatt <oriporatt@gmail.com>
2025-03-23 16:17:47 +0200

    updated FULL

--- frontend/src/serviceWorkerRegistration.js
+++ frontend/src/serviceWorkerRegistration.js
@@ -30,7 +30,7 @@ const isLocalhost = Boolean(
       }
   
       window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/backend/service-worker.js`;
+        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
         
         if (isLocalhost) {
